caadaabd194614a4be55ba4631d3b02596c22cce
SilentPenguin <silentpenguin@hotmail.co.uk>
2013-04-22 19:02:47 +0100

    Added ChannelUser

---
+++ Skyscraper.Models/ChannelUser.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skyscraper.Models
+{
+    public class ChannelUser : IChannelUser
+    {
+        public IUser User
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string Modes
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
---
+++ Skyscraper.Models/IChannelUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Skyscraper.Models
+{
+    public interface IChannelUser
+    {
+        IUser User { get; }
+        string Modes { get; set; }
+    }
+}
