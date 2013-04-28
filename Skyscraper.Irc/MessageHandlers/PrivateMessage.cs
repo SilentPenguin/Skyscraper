3f1c2aa81268ae8211c090a9bc8675346e82896d
SilentPenguin <silentpenguin@hotmail.co.uk>
2013-04-28 18:52:07 +0100

    Message Handler

---
+++ Skyscraper.Irc/MessageHandlers/PrivateMessage.cs
@@ -0,0 +1,18 @@
+﻿using Skyscraper.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skyscraper.Irc.MessageHandlers
+{
+    [Handler("PRIVMSG")]
+    class PrivateMessage : IMessageHandler
+    {
+        public void Execute(IMessage message, IConnection connection)
+        {
+            
+        }
+    }
+}
